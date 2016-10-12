09e9f01496571461a551a60971874ce55b2e3964
scott123180 <scott123180@gmail.com>
2016-10-11 23:31:55 -0400

    working on map constructor

--- stochastic/map.cpp
+++ stochastic/map.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+#include "person.h" //for storing person objects
+
+class map(unsigned int width, unsigned int height) {
+
+  Person map[width][height];
+   
+
+}
 //the map object, we'll start out as a matrix for now
