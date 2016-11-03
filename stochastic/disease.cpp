95eb4f28a38125d5164668a06219d60c83fd452d
alykkehoy <alykkeho@purdue.edu>
2016-11-03 06:29:19 -0400

    Added an incubation time to disease for seir model. Added missing set functions to disease.

--- stochastic/disease.cpp
+++ stochastic/disease.cpp
@@ -1,12 +1,22 @@
 #include "disease.h"
 
 //In Disease class, implement the Disease ctor
-Disease::Disease(std::string n, double a, double b, double m_r, int r){
+Disease::Disease(std::string n, double a, double b, double m_r, int r, int time) {
 	range = r;
 	name = n;
 	alpha = a; //average infectious period (aka 1/alpha)
 	beta = b; //contact rate
 	mortality_rate = m_r;
+	incubation_time = time;
+}
+
+Disease::Disease(std::string n, double a, double b, double m_r, int r) {
+	range = r;
+	name = n;
+	alpha = a; //average infectious period (aka 1/alpha)
+	beta = b; //contact rate
+	mortality_rate = m_r;
+	incubation_time = 0;
 }
 
 Disease::Disease(std::string n, double a, double b, double m_r) {
@@ -16,6 +26,7 @@ Disease::Disease(std::string n, double a, double b, double m_r) {
 	alpha = a; //average infectious period (aka 1/alpha)
 	beta = b; //contact rate
 	mortality_rate = m_r;
+	incubation_time = 0;
 }
 
 
@@ -38,4 +49,28 @@ int Disease::get_range() {
 
 std::string Disease::getName(){
   return name;
+}
+
+int Disease::get_incubation_time() {
+	return incubation_time;
+}
+
+void Disease::set_alpha(double a) {
+	alpha = a;
+}
+
+void Disease::set_beta(double b) {
+	beta = b;
+}
+
+void Disease::set_mortality_rate(double m_r) {
+	mortality_rate = m_r;
+}
+
+void Disease::set_range(int r) {
+	range = r;
+}
+
+void Disease::set_incubation_time(int t) {
+	incubation_time = t;
 }
--- stochastic/disease.h
+++ stochastic/disease.h
@@ -6,19 +6,30 @@
 class Disease
 {
 public:
-    Disease(std::string n, double a, double b, double m_r, int r);
+    Disease(std::string n, double a, double b, double m_r, int r, int time);
+	Disease(std::string n, double a, double b, double m_r, int r);
 	Disease(std::string n, double a, double b, double m_r);
+
     std::string getName();
     double getAlpha();
     double getBeta();
 	double get_mortality_rate();
 	int get_range();
+	int get_incubation_time();
+
+	void set_alpha(double a);
+	void set_beta(double b);
+	void set_mortality_rate(double m_r);
+	void set_range(int r);
+	void set_incubation_time(int t);
+
 
 private:
 	std::string name;
 	double alpha;
 	double beta;
 	double mortality_rate;
+	int incubation_time;
 	int range;
 };
 
