1d1e0184185b8c5e8b0b4d250cfb2f7c1dc85739
Surya Kiran Akula <surya.akula1@marist.edu>
2016-11-08 17:49:25 +0000

    sirtest.cpp created online with Bitbucket

---
+++ deterministic/sirtest.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+#include<cmath>
+#include "SIR.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+double beta0 = 1.5;
+double gamma0 =0.2;
+
+double I00 = 5;
+double S00 = 45;
+
+cout<<I00<<endl;
+
+double tmax0 = 5;
+
+double step0=0.01/((beta0+gamma0)*S00);
+
+SIR testSIR(beta0,gamma0,step0,S00,I00,tmax0);
+
+testSIR.output();
+return(0);
+
+}
