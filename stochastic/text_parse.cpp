c16fc9bb76dc4adcc2838d976876a9c9466b1fea
Scott123180 <Scott123180@gmail.com>
2016-11-08 13:51:05 -0500

    working on text files

--- stochastic/text_parse.cpp
+++ stochastic/text_parse.cpp
@@ -1,4 +1,39 @@
-#include <iostream>
+#include <sstream>
+#include <string>
 
 //parse input text
+void text_parse::input() //TODO add input file parameter 
+{
+  string iline;
+  while(getline(cin, iline))
+  {
+    istringstream iss(iline);
+    string pname, type, value; 
+    iss >> pname;
+    iss >> type;
+    iss >> value;
+    
+    if(pname == "name")
+    {
+         
+    }
+    else if(pname == "alpha")
+    {
+       
+    }
+    else if(pname == "beta")
+    {
+      
+    }
+    else if(pname == "mortality")
+    {
+      
+    }
+    else if(pname == "range")
+    {
+      
+    }   
+  }  
+}
 
+//functions for conversion of data types
