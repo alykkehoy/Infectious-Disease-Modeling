f5161fb9a761908ad77e28c8c424c2910c6a19f8
Anders Lykkehoy <anders.lykkehoy1@marist.edu>
2016-11-14 08:22:08 -0500

    small fixes to increase custom model speed.

--- stochastic/States/State.h
+++ stochastic/States/State.h
@@ -15,7 +15,7 @@ public:
 	State();
 	~State();
 
-	virtual void take_step(int i, int j, Disease& disease, Map& nextMap, Map& current_map, std::vector<std::shared_ptr<State>> model) = 0;
+	virtual void take_step(int i, int j, Disease& disease, Map& nextMap, Map& current_map) = 0;
 
 	char get_char_rep();
 	void create_next_char(std::vector<std::shared_ptr<State>> model);
