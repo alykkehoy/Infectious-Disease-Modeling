efb6c75158c675f72853f4833e0245bac6d09d18
Anders Lykkehoy <anders.lykkehoy1@marist.edu>
2016-11-18 01:14:29 -0500

    Open gl stuff

---
+++ stochastic/OpenGL stuff/Shader.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include<GL\glew.h>
+
+class Shader
+{
+public:
+	Shader(const std::string& fileName);
+	~Shader();
+	void Bind();
+	std::string LoadShader(const std::string& fileName);
+	void CheckShaderError(GLuint shader, GLuint flag, bool isProgram, const std::string& errorMessage);
+	GLuint CreateShader(const std::string& text, GLenum shaderType);
+
+
+private:
+	static const unsigned int NUM_SHADERS = 2;
+	GLuint m_progam;
+	GLuint m_sharders[NUM_SHADERS];
+};
+
--- stochastic/main.cpp
+++ stochastic/main.cpp
@@ -40,30 +40,16 @@ int main(){
   int time = 50;
   int num_seeds = 1;
 
-  analytics a("Analytics", pop_width  * pop_height, num_seeds);
-  //a.set_pop_size(pop_width  * pop_height);
-  //a.set_num_seeds(num_seeds);
-
   Disease disease("test", 3, 30, 10, 1, 2);
 
   Map map(pop_width, pop_height);
   for (int i = 0; i < num_seeds; i++) {
 	  map.random_seed(disease);
   }
-  //map.random_seed_immunity(30);
-
-  SIR sir;
-  SIRS sirs;
-  SEIR seir;
-
-  //std::ofstream outfile("filename");
   
   for (int loops = 0; loops < time; loops++) {
 	  std::cout << "Time: " << loops << std::endl;
 	  map.print_map();
-	  //map = sir.take_step(disease, a, map);
-	  //map = sirs.take_step(disease, a, map);
-	  //map = seir.take_step(disease, a, map);
 	  map = m_sir.take_step(disease, map);
   }
 
@@ -72,11 +58,6 @@ int main(){
 
   map.print_immunity_map();
   map.print_num_infected_map();
-
-  //a.calc_analytics(map);
-  //a.print_delta_analytics();
-  //a.print_num_analytics();
-  //a.print_avg();
 
   return 0;
 }
