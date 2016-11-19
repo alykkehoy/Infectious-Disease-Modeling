168597de16e960edbd1e78b401af77ea0b7d2e1b
Scott123180 <Scott123180@gmail.com>
2016-11-19 16:19:21 -0500

    working on input files

--- stochastic/text_parse.cpp
+++ stochastic/text_parse.cpp
@@ -1,6 +1,7 @@
 #include <sstream>
 #include <string>
 #include <iostream>
+#include "text_parse.h"
 
 using std::istringstream;
 using std::cout;
@@ -8,13 +9,13 @@ using std::string;
 using std::cin;
 
 //parse input text
-void Text_parse::Text_parse() //TODO add input file parameter
+void Text_parse::Text_parse(string iline) //TODO add input file parameter
 {
-  string iline;
   while(getline(cin, iline))
   {
     istringstream iss(iline);
-    string pname, type, value; 
+    string pname;
+    auto value;
     iss >> pname;
     iss >> value;
 
@@ -64,4 +65,84 @@ void Text_parse::Text_parse() //TODO add input file parameter
     }
 
   }
+}
+
+const string &Text_parse::getDiseaseName() const {
+    return diseaseName;
+}
+
+void Text_parse::setDiseaseName(const string &diseaseName) {
+    Text_parse::diseaseName = diseaseName;
+}
+
+const string &Text_parse::getDiseaseModel() const {
+    return diseaseModel;
+}
+
+void Text_parse::setDiseaseModel(const string &diseaseModel) {
+    Text_parse::diseaseModel = diseaseModel;
+}
+
+int Text_parse::getAlpha() const {
+    return alpha;
+}
+
+void Text_parse::setAlpha(int alpha) {
+    Text_parse::alpha = alpha;
+}
+
+int Text_parse::getBeta() const {
+    return beta;
+}
+
+void Text_parse::setBeta(int beta) {
+    Text_parse::beta = beta;
+}
+
+int Text_parse::getMortality() const {
+    return mortality;
+}
+
+void Text_parse::setMortality(int mortality) {
+    Text_parse::mortality = mortality;
+}
+
+int Text_parse::getRange() const {
+    return range;
+}
+
+void Text_parse::setRange(int range) {
+    Text_parse::range = range;
+}
+
+int Text_parse::getPop_width() const {
+    return pop_width;
+}
+
+void Text_parse::setPop_width(int pop_width) {
+    Text_parse::pop_width = pop_width;
+}
+
+int Text_parse::getPop_height() const {
+    return pop_height;
+}
+
+void Text_parse::setPop_height(int pop_height) {
+    Text_parse::pop_height = pop_height;
+}
+
+int Text_parse::getSim_len() const {
+    return sim_len;
+}
+
+void Text_parse::setSim_len(int sim_len) {
+    Text_parse::sim_len = sim_len;
+}
+
+long Text_parse::getSeed() const {
+    return seed;
+}
+
+void Text_parse::setSeed(long seed) {
+    Text_parse::seed = seed;
 }
--- stochastic/text_parse.h
+++ stochastic/text_parse.h
@@ -3,14 +3,48 @@
 
 #include <string>
 
+using std::string;
+
 class Text_parse
 {
-public:
+private:
     //disease parameters //
     string diseaseName, diseaseModel;
     int alpha, beta, mortality, range, pop_width, pop_height, sim_len;
     long seed;
-private:
+
+public:
+    Text_parse(string);
+    //getters and setters //
+    const string &getDiseaseName() const;
+    void setDiseaseName(const string &diseaseName);
+
+    const string &getDiseaseModel() const;
+    void setDiseaseModel(const string &diseaseModel);
+
+    int getAlpha() const;
+    void setAlpha(int alpha);
+
+    int getBeta() const;
+    void setBeta(int beta);
+
+    int getMortality() const;
+    void setMortality(int mortality);
+
+    int getRange() const;
+    void setRange(int range);
+
+    int getPop_width() const;
+    void setPop_width(int pop_width);
+
+    int getPop_height() const;
+    void setPop_height(int pop_height);
+
+    int getSim_len() const;
+    void setSim_len(int sim_len);
+
+    long getSeed() const;
+    void setSeed(long seed);
 
 };
 #endif //GUARD_TEXT_PARSE_H
