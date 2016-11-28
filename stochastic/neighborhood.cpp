178de3246c9b05e834cde3f40cd0c1f178de2450
Scott123180 <Scott123180@gmail.com>
2016-11-28 00:20:51 -0500

    circle

--- stochastic/neighborhood.cpp
+++ stochastic/neighborhood.cpp
@@ -14,16 +14,24 @@ vector<pair<unsigned int, unsigned int>> Neighborhood::findNeighbors(int x, int 
 
   //error checking
   bool error = false;
+
   if(map.get_rows() < (magnitude * 2)) //magnitude is doubled in calculation below(i.e. radius)
   {
     cerr << "Check your dimensions. Height of map is less than transmission area height" << endl;
     error = true;
   }
+
   if(map.get_cols() < (magnitude * 2)) //magnitude is doubled in calculation below(i.e. radius)
   {
-    cerr << "Error opening parameter file for simulation" << endl;
+    cerr << "Check your dimensions. Width of map is less than transmission area width" << endl;
     error = true;
   }
+
+  if(magnitude < 0) //negative distance
+  {
+    cerr << "Check your magnitude, it's less than zero" << endl;
+  }
+
   if(error) exit(1); //exit the program
 
 
@@ -39,17 +47,24 @@ vector<pair<unsigned int, unsigned int>> Neighborhood::findNeighbors(int x, int 
   }
 }
 
-vector <pair<unsigned int, unsigned int>>Neighborhood::calculate_Circle()
+
+vector <pair<unsigned int, unsigned int>>Neighborhood::calculate_Circle(x, y, magnitude)
+{
+  //Brensenham's midpoint circle algorithm
+  int e = -1 * magnitude; //E = -R
+  int x = magitude; //X = R
+  int y = 0; //Y = 0
+
+
+
+}
+
+vector <pair<unsigned int, unsigned int>>Neighborhood::calculate_Adjacent(x, y, magnitude)
 {
 
 }
 
-vector <pair<unsigned int, unsigned int>>Neighborhood::calculate_Adjacent()
-{
-
-}
-
-vector <pair<unsigned int, unsigned int>>Neighborhood::calculate_Square()
+vector <pair<unsigned int, unsigned int>>Neighborhood::calculate_Square(x, y, magnitude)
 {
 
 }
