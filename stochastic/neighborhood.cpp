5d5a1f8a00337b339f4e88d3d29ca937cf67c263
Scott123180 <Scott123180@gmail.com>
2016-11-28 23:22:27 -0500

    finished adjacent neighbors

--- stochastic/neighborhood.cpp
+++ stochastic/neighborhood.cpp
@@ -6,22 +6,24 @@ using std::vector;
 using std::pair;
 using std::endl;
 using std::cerr;
+using std::make_pair;
 
 //neighborhood object, will return what the neighborhood for a cell is
 vector<pair<unsigned int, unsigned int>> Neighborhood::findNeighbors(int x, int y,
                                                       int magnitude, area n, Map& map)
 {
-
+  unsigned int h = map.get_rows(); //height
+  unsigned int w = map.get_cols(); //width
   //error checking
   bool error = false;
 
-  if(map.get_rows() < (magnitude * 2)) //magnitude is doubled in calculation below(i.e. radius)
+  if(h < (magnitude * 2)) //magnitude is doubled in calculation below(i.e. radius)
   {
     cerr << "Check your dimensions. Height of map is less than transmission area height" << endl;
     error = true;
   }
 
-  if(map.get_cols() < (magnitude * 2)) //magnitude is doubled in calculation below(i.e. radius)
+  if(w < (magnitude * 2)) //magnitude is doubled in calculation below(i.e. radius)
   {
     cerr << "Check your dimensions. Width of map is less than transmission area width" << endl;
     error = true;
@@ -39,16 +41,16 @@ vector<pair<unsigned int, unsigned int>> Neighborhood::findNeighbors(int x, int 
   switch(n)
   {
     case circle:
-      return Neighborhood::calculate_Circle(x, y, magnitude);
-    case adjacent:
-      return Neighborhood::calculate_Adjacent(x, y, magnitude);
+      return Neighborhood::calculate_Circle(x, y, h, w, magnitude);
+    case adjacent: //right next to infected (no diagonals)
+      return Neighborhood::calculate_Adjacent(x, y, h, w);
     case square:
-      return Neighborhood::calculate_Square(x, y, magnitude);
+      return Neighborhood::calculate_Square(x, y, h, w, magnitude);
   }
 }
 
 
-vector <pair<unsigned int, unsigned int>>Neighborhood::calculate_Circle(x, y, magnitude)
+vector <pair<unsigned int, unsigned int>>Neighborhood::calculate_Circle(x, y, h, w, magnitude)
 {
   //Brensenham's midpoint circle algorithm
   int e = -1 * magnitude; //E = -R
@@ -59,12 +61,27 @@ vector <pair<unsigned int, unsigned int>>Neighborhood::calculate_Circle(x, y, ma
 
 }
 
-vector <pair<unsigned int, unsigned int>>Neighborhood::calculate_Adjacent(x, y, magnitude)
+vector <pair<unsigned int, unsigned int>>Neighborhood::calculate_Adjacent(x, y, h, w)
 {
+  vector<pair<unsigned int, unsigned int>> n; //neighbors
 
+  //up (y++)
+  if((y + 1) > h) n.push_back(make_pair(x, 0)); //at top of map, up neighbor is bottom
+  else n.push_back(make_pair(x, ++y));
+  //down
+  if((y - 1) < 0) n.push_back(make_pair(x, h)); //at bottom of map, down neighbor is top
+  else n.push_back(make_pair(x, --y));
+  //left
+  if ((x - 1) < 0) n.push_back(make_pair(w, y)); //at left of map, left neighbor is right
+  else n.push_back(make_pair(--x, y));
+  //right
+  if ((x + 1) > w) n.push_back(make_pair(0, y)); //at right of map, right neighbor is left
+  else n.push_back(make_pair(++x, y));
+
+  return n;
 }
 
-vector <pair<unsigned int, unsigned int>>Neighborhood::calculate_Square(x, y, magnitude)
+vector <pair<unsigned int, unsigned int>>Neighborhood::calculate_Square(x, y, h, w, magnitude)
 {
 
 }
